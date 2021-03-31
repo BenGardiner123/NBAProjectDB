4456766d61598d6405c9b8066b864ce2ffd5cb0f
Ben Gardiner <bennygards@gmail.com>
2021-03-31 21:53:30 +1100

    adding the tables, stored proc and view

---
+++ nbaDBv3/PlayerSelection.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[PlayerSelection]
+(
+	[TeamName]          NVARCHAR(50)   Not Null, 
+    [Season]            INT            NOT NULL,
+    [PLAYER_ID]         INT            NOT NULL,
+    primary key (TeamName, Season, Player_Id),
+    Foreign key (TeamName) references Team,
+    Foreign key (Season, Player_ID) references Player
+);
---
+++ nbaDBv3/allPlayers.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW [dbo].[allPlayers] as 
+select FIRSTNAME, Lastname ,AGE, GP, MINS, PLUS_MINUS, AST, BLK, BLKA, OREB, DREB, FG_PCT, 
+FG3_PCT, FG3A, FG3M, FGA, FGM, FT_PCT, FTA, FTM, 
+W, L, W_PCT, PF, PFD
+from (select Season, FIRSTNAME, LASTNAME, TEAM_ABBREVIATION ,AGE, GP, MINS, PLUS_MINUS, AST, BLK, BLKA, OREB, DREB, FG_PCT, 
+FG3_PCT, FG3A, FG3M, FGA, FGM, FT_PCT, FTA, FTM, 
+W, L, W_PCT, PF, PFD, RANK() over (PARTITION by PLAYER_ID order by season DESC) n
+from Player
+)m where n = 1
