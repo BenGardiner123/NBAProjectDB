64688e7e407447a0fbd3ef6edcc22902323fc8c0
Ben Gardiner <bennygards@gmail.com>
2021-05-03 22:53:41 +1000

    adding the users table

---
+++ nbaDBv3/Tables/Users.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	 UserID INT IDENTITY(1,1)
+	,UserName VARCHAR(30) NOT NULL CHECK (DATALENGTH(userName) > 0) 
+	,PasswordHash varbinary(max)
+	,PasswordSalt varbinary(max)
+	primary key (UserID)
+
+);
