cdce74e1ce22c663e88f2509888aa41a1a9bf1a5
Ben Gardiner <bennygards@gmail.com>
2021-06-11 16:48:03 +1000

    build varibles not defined ugh,... why
    do they duisappear?

--- nbaDBv3/Tables/Users.sql
+++ nbaDBv3/Tables/Users.sql
@@ -1,10 +1,5 @@
 ﻿CREATE TABLE [dbo].[Users]
 (
-	 UserID INT IDENTITY(1,1)
-	,FirstName VARCHAR(30) 
-	,LastName VARCHAR(30) 
-	,UserName VARCHAR(30) 
-	,PasswordHash varbinary(max)
-	,PasswordSalt varbinary(max)
+	UserID INT IDENTITY(1,1)
 	primary key (UserID)
 );
